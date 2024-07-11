e61c0dbf5ce7b35d8aee7f10ca30e2cddbf5c3fe
Anastasia Savina <savnastya26@yandex.ru>
2024-07-12 02:51:56 +0300

    feat: made project

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -2,18 +2,27 @@ import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
 
-/** Функция для обработки открытия/закрытия формы */
-export type OnClick = () => void;
+import clsx from 'clsx';
+import { MouseEvent } from 'react';
 
-export const ArrowButton = () => {
+/** Функция для обработки открытия/закрытия формы */
+export type OnClick = ( e: MouseEvent<HTMLDivElement>) => void;
+
+type arrowButtonProps = {
+	onClick: OnClick;
+	isOpen: boolean;
+}
+
+export const ArrowButton = ({onClick, isOpen}: arrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
+		    onClick={onClick}
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, {[styles.container_open] : isOpen})}>
+			<img src={arrow} alt='иконка стрелочки' className={clsx(styles.arrow, {[styles.arrow_open] : isOpen})} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -3,15 +3,94 @@ import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+import { MouseEvent, useState, useRef, FormEvent } from 'react';
+import { ArticleStateType, OptionType, backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import { Text } from '../text';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
+import clsx from 'clsx';
+
+type articleParamsFormProps = {
+	setStatusPage: (state: ArticleStateType) => void
+}
+
+export const ArticleParamsForm = ({setStatusPage}: articleParamsFormProps) => {
+
+	const [isOpen, setIsOpen] = useState(false);
+	const rootRef = useRef(null);
+	const [edition, setEdition] = useState(defaultArticleState);
+
+	const handleArrowClick = (e: MouseEvent) => {
+		e.stopPropagation();
+		setIsOpen(!isOpen)
+	}
+
+	const onSubmit = (evt: FormEvent) => {
+		evt.preventDefault();
+	};
+
+	const handleResetPage = () => {
+		setStatusPage(defaultArticleState);
+	}
+
+	const updateArticleState = (key: keyof ArticleStateType, option: OptionType) => {
+		setEdition((prev) => ({
+			...prev,
+			[key]: option
+		}))
+	}
+
+	useOutsideClickClose({isOpen, onClose: () => setIsOpen(false), rootRef, onChange: () => setIsOpen(false)})
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={handleArrowClick} isOpen={isOpen} />
+			<aside ref={rootRef} className={clsx(styles.container, {[styles.container_open]: isOpen})}>
+				<form className={styles.form} onSubmit={onSubmit} onReset={handleResetPage}>
+
+					<Text weight={800} size={31} align='left' uppercase={true}>Задайте параметры</Text>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={edition.fontFamilyOption}
+						onChange={(option) => {
+							updateArticleState('fontFamilyOption', option);
+						}}
+					/>
+					<RadioGroup
+						name='size_button'
+						title='Размер шрифта'
+						selected={edition.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) => {
+							updateArticleState('fontSizeOption', option);
+						}}
+					/>
+					<Select
+						title='Цвет шрифта'
+						selected={edition.fontColor}
+						options={fontColors}
+						onChange={(option) => updateArticleState('fontColor', option)}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						selected={edition.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => updateArticleState('backgroundColor', option)}
+					/>
+					<Select
+						title='Ширина контента'
+						selected={edition.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => updateArticleState('contentWidth', option)}
+					/>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title='Сбросить' type='reset' onClick={() => setEdition(defaultArticleState)} />
+						<Button title='Применить' type='submit' onClick={() => setStatusPage(edition)} />
 					</div>
 				</form>
 			</aside>
